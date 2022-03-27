15d726297d352c646cc48f0a20e54ab5ea1f60b3
Daniil <Smirnov_Danya_2001@mail.ru>
2022-03-27 22:08:49 +0300

    task-objects

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,19 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    switch (data.gender) {
+        case 'female':
+            delete data.age;
+            break;
+        case 'male':
+            if (!!data.income) {
+                null;
+            } else {
+                data.income = 100000;
+            }
+            break;
+    }
+    return data;
 }
 
 /*
@@ -15,6 +28,11 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    return [
+        ...Object.keys(obj1),
+        ...Object.keys(obj2),
+        ...Object.keys(obj3),
+    ].sort();
 }
 
 /*
@@ -23,4 +41,11 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let mas = [];
+    for (let i = 0; i < count; i++) {
+        let objec = JSON.parse(JSON.stringify(obj));
+        objec.id = i;
+        mas.push(objec);
+    }
+    return mas;
 }
